5e41e007d9e222b8192c97e3aa03a4635eeac771
JTLiner925 <[jtliner925@gmail.com]>
2020-04-21 08:49:37 -0500

    added store file, set up router in index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
+import dummy-store from "./dummy-store"
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
       
     </div>
   );
+}
 }
 
 export default App;
